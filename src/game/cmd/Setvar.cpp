f107c773363d69e6a77f22b23a60ae7e4b105064
andyyy78 <andy@stutzmanplace.com>
2017-02-11 21:07:48 -0800

    Fast Panzer related changes

--- src/game/cmd/Setvar.cpp
+++ src/game/cmd/Setvar.cpp
@@ -7,7 +7,10 @@ namespace cmd {
 Setvar::Setvar()
     : AbstractBuiltin( "setvar" )
 {
-    __usage << xvalue( "!" + _name ) << " [" << xvalue( "fastpanzer" ) << ']' << " [0 - 45]\n" << "Sets death count for custom fast panzer calculations - the higher the faster, up to around 45";
+    __usage << xvalue( "!" + _name ) << " [" << xvalue( "fastpanzer" ) << ']' << " [" << xvalue("25 - 10000") << "]\n" 
+		<< "Sets panzer reload time in milliseconds\n"
+		<< xvalue( "!" + _name ) << " [" << xvalue( "panzerrecoil" ) << ']' << " <" << xvalue("on") << "|" << xvalue("off") << ">\n"
+		<< "Turns on/off panzer recoil setting\n";
     __descr << "Sets values for different session variables.";
 }
 
@@ -46,17 +49,19 @@ Setvar::doExecute( Context& txt )
 		// Can't use string here because ubuntu compiler isn't version 11 so doesn't
 		// support stoi() method (string to int).  Have to use older atoi() which requires
 		// const char*
-		const char *deathstoset = txt._args[2].c_str();
-		int iDeathsToSet = atoi(deathstoset);
-		if (iDeathsToSet >= 0 && iDeathsToSet <= 10000)
+		//const char *panzerreloadtimems = txt._args[2].c_str();
+	    string panzerreloadtimems = txt._args[2];
+	    str::toLower( panzerreloadtimems );
+		int ipanzerreloadtimems = atoi(panzerreloadtimems.c_str());
+		if (ipanzerreloadtimems >= 25 && ipanzerreloadtimems <= 10000)
 		{
-			txt._client->gclient.sess.deathsforpanzerreload = iDeathsToSet;
+			txt._client->gclient.sess.panzerreloadtimems = ipanzerreloadtimems;
 			// Notify user changes have been made
-			buf << _name << ": fastpanzer count has been set to " << xvalue( iDeathsToSet ) << '.';
+			buf << _name << ": fastpanzer reload time has been set to " << xvalue( va("%i", ipanzerreloadtimems) ) << "ms.";
 			printCpm( txt._client, buf, true );
 		}
 		else {
-			txt._ebuf << "Invalid fastpanzer count (should be between 0 and 10000, though realistically only uses value as high as 45): " << xvalue( txt._args[2] );
+			txt._ebuf << "Invalid fastpanzer reload time (should be between 25 and 10000): " << xvalue( txt._args[2] );
 			return PA_ERROR;
 		}
     }
@@ -64,9 +69,11 @@ Setvar::doExecute( Context& txt )
 		// Can't use string here because ubuntu compiler isn't version 11 so doesn't
 		// support stoi() method (string to int).  Have to use older atoi() which requires
 		// const char*
-		const char *deathstoset = txt._args[2].c_str();
-		int iDeathsToSet = atoi(deathstoset);
-		if (iDeathsToSet >= 0 && iDeathsToSet <= 10000)
+		//const char *panzerreloadtimems = txt._args[2].c_str();
+	    string panzerreloadtimems = txt._args[2];
+	    str::toLower( panzerreloadtimems );
+		int ipanzerreloadtimems = atoi(panzerreloadtimems.c_str());
+		if (ipanzerreloadtimems >= 25 && ipanzerreloadtimems <= 10000)
 		{
 			Client* target;
 			if (lookupPLAYER( txt._args[3], txt, target ))
@@ -77,13 +84,13 @@ Setvar::doExecute( Context& txt )
 
 			const User& targetUser = *connectedUsers[target->slot];
 			gentity_t* const targetEnt = &target->gentity;
-			targetEnt->client->sess.deathsforpanzerreload = iDeathsToSet;
+			targetEnt->client->sess.panzerreloadtimems = ipanzerreloadtimems;
 			// Notify user changes have been made
-			buf << _name << ": fastpanzer count has been set to " << xvalue( iDeathsToSet ) << '.';
+			buf << _name << ": fastpanzer reload time has been set to " << xvalue( va("%i", ipanzerreloadtimems) ) << "ms.";
 			printCpm( txt._client, buf, true );
 		}
 		else {
-			txt._ebuf << "Invalid fastpanzerplayer count (should be between 0 and 10000, though realistically only uses value as high as 45): " << xvalue( txt._args[2] );
+			txt._ebuf << "Invalid fastpanzerplayer reload time (should be between 25 and 10000): " << xvalue( txt._args[2] );
 			return PA_ERROR;
 		}
     }
